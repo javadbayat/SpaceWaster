c25bb33ef9f1797894490d47742afac79be6589f
javadbayat <31417320+javadbayat@users.noreply.github.com>
2023-06-28 01:04:46 +0330

    Add files via upload

---
+++ ui/mainForm.js
@@ -0,0 +1,37 @@
+document.onselectstart = document.oncontextmenu = function() {
+    var srcElem = event.srcElement;
+    
+    if ((srcElem.tagName.toLowerCase() == "input") && (srcElem.type.toLowerCase() == "text"))
+        return true;
+    
+    if (srcElem.tagName.toLowerCase() == "textarea")
+        return true;
+    
+    if (srcElem.tagName.toLowerCase() == "intbox")
+        return true;
+    
+    return false;
+};
+
+function setUIEventHandlers() {
+    document.mainForm.writeConstantBytes.onclick = function() {
+        intByteValue.cdisabled = !this.checked;
+    };
+}
+
+function switchToOperationStatus() {
+    document.mainForm.btnWaste.style.visibility = "hidden";
+    progressBar.style.display = "";
+    progress.style.visibility = "visible";
+    document.all.btnAbort.style.visibility = "visible";
+}
+
+function switchToInputIdle() {
+    progressBar.filters[0].Enabled = false;
+    progressBar.style.display = "none";
+    progress.style.visibility = "hidden";
+    document.all.btnAbort.style.visibility = "hidden";
+    document.mainForm.btnWaste.style.visibility = "visible";
+    progress.innerText = "";
+    document.title = appName;
+}
