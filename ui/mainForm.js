37838ea5915b97db7e444f05f8944a9abb8e33bc
javadbayat <31417320+javadbayat@users.noreply.github.com>
2023-07-04 18:56:29 +0330

    Add files via upload

--- ui/mainForm.js
+++ ui/mainForm.js
@@ -26,10 +26,21 @@ function switchToOperationStatus() {
     document.all.btnAbort.style.visibility = "visible";
 }
 
+function switchToZippingStatus() {
+    progressBar.filters[0].Enabled = false;
+    progressBar.style.display = "none";
+    progress.style.visibility = "hidden";
+    document.all.btnAbort.style.visibility = "hidden";
+    
+    document.all.zipProgBar.style.display = "";
+    document.title = appName + " (Compressing)";
+}
+
 function switchToInputIdle() {
     progressBar.filters[0].Enabled = false;
     progressBar.style.display = "none";
     progress.style.visibility = "hidden";
+    document.all.zipProgBar.style.display = "none";
     document.all.btnAbort.style.visibility = "hidden";
     document.mainForm.btnWaste.style.visibility = "visible";
     progress.innerText = "";
